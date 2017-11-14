093259bf3d305045290f38510069ebd4271e5505
gfdvvdfv <licheng1028@126.com>
2017-11-14 18:34:46 +0800

    电脑跳转手机

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -17,6 +17,27 @@
   <!--[if lt IE 9]><!-->
   <script src="js/html5shiv.min.js"></script>
   <script src="js/respond.min.js"></script>
+  <script type="text/javascript">
+      //平台、设备和操作系统
+      var system = {
+          win: false,
+          mac: false,
+          xll: false,
+          ipad:false
+      };
+      //检测平台
+      var p = navigator.platform;
+      system.win = p.indexOf("Win") == 0;
+      system.mac = p.indexOf("Mac") == 0;
+      system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+      system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+      //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
+      if (system.win || system.mac || system.xll||system.ipad) {
+
+      } else {
+          window.location.href = "/wap";
+      }
+  </script>
   <!--[endif]-->
   <!-- 先后 -->
 </head>
