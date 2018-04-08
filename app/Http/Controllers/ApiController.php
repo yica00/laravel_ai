c9d5a6f5814c4186be5358c528a2fc66bda61cea
licheng1028@126.com <li1029>
2018-04-08 16:58:26 +0800

    新增添加用户

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,6 +7,7 @@ use App\Models\Admin\Teams;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Input;
 
 class ApiController extends Controller
